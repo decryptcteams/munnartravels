aa03ccae613d2026d9671f2237d2bde7086c5c17
decryptc <decryptc.teams@gmail.com>
2025-12-21 10:15:59 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  },
-  resolve: {
-    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
-  }
+  base: './'
 });
